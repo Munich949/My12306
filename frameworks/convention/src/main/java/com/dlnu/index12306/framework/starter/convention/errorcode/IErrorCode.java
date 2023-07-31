5c9140ea92cd0e0249ef1301bdb8c7521db94ade
Munich <2493929776@qq.com>
2023-07-31 20:24:28 +0800

    feature: 规约组件模块

---
+++ frameworks/convention/src/main/java/com/dlnu/index12306/framework/starter/convention/errorcode/IErrorCode.java
@@ -0,0 +1,16 @@
+package com.dlnu.index12306.framework.starter.convention.errorcode;
+
+/**
+ * 平台错误码
+ */
+public interface IErrorCode {
+    /**
+     * 错误码
+     */
+    String code();
+
+    /**
+     * 错误信息
+     */
+    String message();
+}
