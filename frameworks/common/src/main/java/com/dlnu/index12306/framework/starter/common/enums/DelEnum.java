91734f3cfae1ffbc050a32994a11bc33ae927aa1
Munich <2493929776@qq.com>
2023-08-03 15:59:55 +0800

    feature: 完成公共组件库

---
+++ frameworks/common/src/main/java/com/dlnu/index12306/framework/starter/common/enums/DelEnum.java
@@ -0,0 +1,30 @@
+package com.dlnu.index12306.framework.starter.common.enums;
+
+/**
+ * 删除标记枚举
+ */
+public enum DelEnum {
+    /**
+     * 正常状态
+     */
+    NORMAL(0),
+    DELETE(1);
+    private final Integer statusCode;
+
+    DelEnum(Integer statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public Integer code() {
+        return this.statusCode;
+    }
+
+    public String strCode() {
+        return String.valueOf(this.statusCode);
+    }
+
+    @Override
+    public String toString() {
+        return strCode();
+    }
+}
---
+++ frameworks/common/src/main/java/com/dlnu/index12306/framework/starter/common/enums/OperationTypeEnum.java
@@ -0,0 +1,13 @@
+package com.dlnu.index12306.framework.starter.common.enums;
+
+/**
+ * 操作类型
+ */
+public enum OperationTypeEnum {
+
+    SAVE,
+
+    UPDATE,
+
+    DELETE
+}
