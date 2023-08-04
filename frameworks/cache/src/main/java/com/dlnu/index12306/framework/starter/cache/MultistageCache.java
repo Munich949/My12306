7a14d9b705b855151437ae136a6bad0b2375d658
Munich <2493929776@qq.com>
2023-08-04 17:51:05 +0800

    feature: 完成消息队列幂等组件库 feature: 完成Web组件库

---
+++ frameworks/cache/src/main/java/com/dlnu/index12306/framework/starter/cache/MultistageCache.java
@@ -0,0 +1,7 @@
+package com.dlnu.index12306.framework.starter.cache;
+
+/**
+ * 多级缓存
+ */
+public interface MultistageCache extends Cache {
+}
---
+++ frameworks/idempotent/src/main/java/com/dlnu/index12306/framework/starter/idempotent/core/param/IdempotentParamService.java
@@ -0,0 +1,9 @@
+package com.dlnu.index12306.framework.starter.idempotent.core.param;
+
+import com.dlnu.index12306.framework.starter.idempotent.core.IdempotentExecuteHandler;
+
+/**
+ * 参数方式幂等实现接口
+ */
+public interface IdempotentParamService extends IdempotentExecuteHandler {
+}
