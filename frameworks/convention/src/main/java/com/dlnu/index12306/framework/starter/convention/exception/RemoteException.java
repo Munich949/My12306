4782d49f802471fd9b03440a007b11516ee6d5f6
Munich <2493929776@qq.com>
2023-08-16 16:09:28 +0800

    feature: 扩展远程调用异常构造函数

--- frameworks/convention/src/main/java/com/dlnu/index12306/framework/starter/convention/exception/RemoteException.java
+++ frameworks/convention/src/main/java/com/dlnu/index12306/framework/starter/convention/exception/RemoteException.java
@@ -1,11 +1,16 @@
 package com.dlnu.index12306.framework.starter.convention.exception;
 
+import com.dlnu.index12306.framework.starter.convention.errorcode.BaseErrorCode;
 import com.dlnu.index12306.framework.starter.convention.errorcode.IErrorCode;
 
 /**
  * 远程服务调用异常
  */
 public class RemoteException extends AbstractException {
+
+    public RemoteException(String message) {
+        this(message, null, BaseErrorCode.REMOTE_ERROR);
+    }
 
     public RemoteException(String message, IErrorCode errorCode) {
         this(message, null, errorCode);
