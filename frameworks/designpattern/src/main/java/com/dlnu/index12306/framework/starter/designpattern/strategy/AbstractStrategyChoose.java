c2645b5ad8b824852d83fb12de001ec21d12391e
Munich <2493929776@qq.com>
2023-08-20 15:30:17 +0800

    feature: 抽象策略模式适配范解析组件标识

--- frameworks/designpattern/src/main/java/com/dlnu/index12306/framework/starter/designpattern/strategy/AbstractStrategyChoose.java
+++ frameworks/designpattern/src/main/java/com/dlnu/index12306/framework/starter/designpattern/strategy/AbstractStrategyChoose.java
@@ -4,10 +4,12 @@ import com.dlnu.index12306.framework.starter.bases.ApplicationContextHolder;
 import com.dlnu.index12306.framework.starter.bases.init.ApplicationInitializingEvent;
 import com.dlnu.index12306.framework.starter.convention.exception.ServiceException;
 import org.springframework.context.ApplicationListener;
+import org.springframework.util.StringUtils;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.regex.Pattern;
 
 /**
  * 策略选择器
@@ -22,11 +24,20 @@ public class AbstractStrategyChoose implements ApplicationListener<ApplicationIn
     /**
      * 根据 mark 查询具体策略
      *
-     * @param mark 策略标识
+     * @param mark          策略标识
+     * @param predicateFlag 匹配范解析标识
      * @return 实际执行策略
      */
-    public AbstractExecuteStrategy choose(String mark) {
-        return Optional.ofNullable(abstractExecuteStrategyMap.get(mark)).orElseThrow(() -> new ServiceException(String.format("[%s] 策略未定义", mark)));
+    public AbstractExecuteStrategy choose(String mark, Boolean predicateFlag) {
+        if (predicateFlag != null && predicateFlag) {
+            return abstractExecuteStrategyMap.values().stream()
+                    .filter(each -> StringUtils.hasText(each.patternMatchMark()))
+                    .filter(each -> Pattern.compile(each.patternMatchMark()).matcher(mark).matches())
+                    .findFirst()
+                    .orElseThrow(() -> new ServiceException("策略未定义"));
+        }
+        return Optional.ofNullable(abstractExecuteStrategyMap.get(mark))
+                .orElseThrow(() -> new ServiceException(String.format("[%s] 策略未定义", mark)));
     }
 
     /**
@@ -37,7 +48,20 @@ public class AbstractStrategyChoose implements ApplicationListener<ApplicationIn
      * @param <REQUEST>    执行策略入参范型
      */
     public <REQUEST> void chooseAndExecute(String mark, REQUEST requestParam) {
-        AbstractExecuteStrategy executeStrategy = choose(mark);
+        AbstractExecuteStrategy executeStrategy = choose(mark, null);
+        executeStrategy.execute(requestParam);
+    }
+
+    /**
+     * 根据 mark 查询具体策略并执行
+     *
+     * @param mark          策略标识
+     * @param requestParam  执行策略入参
+     * @param predicateFlag 匹配范解析标识
+     * @param <REQUEST>     执行策略入参范型
+     */
+    public <REQUEST> void chooseAndExecute(String mark, REQUEST requestParam, Boolean predicateFlag) {
+        AbstractExecuteStrategy executeStrategy = choose(mark, predicateFlag);
         executeStrategy.execute(requestParam);
     }
 
@@ -51,7 +75,7 @@ public class AbstractStrategyChoose implements ApplicationListener<ApplicationIn
      * @return
      */
     public <REQUEST, RESPONSE> RESPONSE chooseAndExecuteResp(String mark, REQUEST requestParam) {
-        AbstractExecuteStrategy executeStrategy = choose(mark);
+        AbstractExecuteStrategy executeStrategy = choose(mark, null);
         return (RESPONSE) executeStrategy.executeResp(requestParam);
     }
 
