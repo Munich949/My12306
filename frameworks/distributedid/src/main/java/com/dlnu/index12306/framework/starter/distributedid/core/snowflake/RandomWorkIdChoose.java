a3bf8fb44bf5b3144db68a3cec23ac23bbc882f7
Munich <2493929776@qq.com>
2023-12-12 23:49:58 +0800

    feature: 分布式ID组件库模块

--- frameworks/distributedid/src/main/java/com/dlnu/index12306/framework/starter/distributedid/core/snowflake/RandomWorkIdChoose.java
+++ frameworks/distributedid/src/main/java/com/dlnu/index12306/framework/starter/distributedid/core/snowflake/RandomWorkIdChoose.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.dlnu.index12306.framework.starter.distributedid.core.snowflake;
 
 import lombok.extern.slf4j.Slf4j;
@@ -9,6 +26,11 @@ import org.springframework.beans.factory.InitializingBean;
 @Slf4j
 public class RandomWorkIdChoose extends AbstractWorkIdChooseTemplate implements InitializingBean {
 
+    private static long getRandom(int start, int end) {
+        long random = (long) (Math.random() * (end - start + 1) + start);
+        return random;
+    }
+
     @Override
     protected WorkIdWrapper chooseWorkId() {
         int start = 0, end = 31;
@@ -18,10 +40,5 @@ public class RandomWorkIdChoose extends AbstractWorkIdChooseTemplate implements 
     @Override
     public void afterPropertiesSet() throws Exception {
         chooseAndInit();
-    }
-
-    private static long getRandom(int start, int end) {
-        long random = (long) (Math.random() * (end - start + 1) + start);
-        return random;
     }
 }
