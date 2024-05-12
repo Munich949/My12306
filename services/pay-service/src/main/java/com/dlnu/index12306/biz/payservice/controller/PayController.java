5e7b19f488d59c820beeb8f4deda4a521a457dba
Munich <yanghaiyu@baiwang.com>
2024-05-12 09:12:24 +0800

    <feature>: 支付模块控制层查询支付单

--- services/pay-service/src/main/java/com/dlnu/index12306/biz/payservice/controller/PayController.java
+++ services/pay-service/src/main/java/com/dlnu/index12306/biz/payservice/controller/PayController.java
@@ -20,6 +20,7 @@ package com.dlnu.index12306.biz.payservice.controller;
 import com.dlnu.index12306.biz.payservice.convert.PayRequestConvert;
 import com.dlnu.index12306.biz.payservice.dto.PayCommand;
 import com.dlnu.index12306.biz.payservice.dto.base.PayRequest;
+import com.dlnu.index12306.biz.payservice.dto.resp.PayInfoRespDTO;
 import com.dlnu.index12306.biz.payservice.dto.resp.PayRespDTO;
 import com.dlnu.index12306.biz.payservice.service.PayService;
 import com.dlnu.index12306.framework.starter.convention.result.Result;
@@ -27,9 +28,7 @@ import com.dlnu.index12306.framework.starter.idempotent.annotation.Idempotent;
 import com.dlnu.index12306.framework.starter.idempotent.enums.IdempotentTypeEnum;
 import com.dlnu.index12306.framework.starter.web.Results;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 支付控制层
@@ -54,5 +53,21 @@ public class PayController {
         PayRequest payRequest = PayRequestConvert.command2PayRequest(requestParam);
         PayRespDTO result = payService.commonPay(payRequest);
         return Results.success(result);
+    }
+
+    /**
+     * 跟据订单号查询支付单详情
+     */
+    @GetMapping("/api/pay-service/pay/query/order-sn")
+    public Result<PayInfoRespDTO> getPayInfoByOrderSn(@RequestParam(value = "orderSn") String orderSn) {
+        return Results.success(payService.getPayInfoByOrderSn(orderSn));
+    }
+
+    /**
+     * 跟据支付流水号查询支付单详情
+     */
+    @GetMapping("/api/pay-service/pay/query/pay-sn")
+    public Result<PayInfoRespDTO> getPayInfoByPaySn(@RequestParam(value = "paySn") String paySn) {
+        return Results.success(payService.getPayInfoByPaySn(paySn));
     }
 }
